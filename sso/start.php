738f615fb028b72353c266ef568cc99f73330e42
Erin3012 <ericxxnn20@gmail.com>
2026-09-23 21:59:15 -0300

    Corregir regreso al panel de mantenciones

--- sso/start.php
+++ sso/start.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 require_once __DIR__ . '/client.php';
-if (current_user()) redirect('panel.php');
+if (current_user()) redirect('../panel.php');
 $state = bin2hex(random_bytes(32)); $_SESSION['sso_state'] = $state;
 $portal = rtrim(env_value('PORTAL_SSO_URL', 'https://portal.metalrubber.cl'), '/');
 redirect($portal . '/sso/authorize.php?client_id=maintenance&state=' . rawurlencode($state));
